4b0bb721eed78b77f86f7101d13f5d0137a9277f
Diego Saavedra León <dleon.desarrollo@gmail.com>
2024-01-16 16:09:51 -0300

    Se configura el archivo vite.config.js para el reconocimiento de bootstrap

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname,'node_modules/bootstrap')
+        }
+    }
 });

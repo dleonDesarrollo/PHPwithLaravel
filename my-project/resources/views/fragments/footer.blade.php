27d3c55fed6e3866d29ef48736e15212914d8852
Diego Saavedra León <dleon.desarrollo@gmail.com>
2024-01-16 13:43:48 -0300

    Se crea fragments blade footer

---
+++ my-project/resources/views/fragments/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- resources/views/fragments/footer.blade.php -->
+<footer class="">
+        <div class="container mx-auto">
+            <p class="text-center">&copy; 2024 Portafolio Desarrolado por Diego Saavedra León. Todos los derechos reservados.</p>
+        </div>
+</footer>

6d2c1f92ac38fb43f19f694f49581a2651f5b795
Diego Saavedra León <dleon.desarrollo@gmail.com>
2024-01-17 23:18:45 -0300

    Definición y llamado de propiedad para footer 'copyrigth' en multi-lenguages

--- my-project/lang/en/messages.php
+++ my-project/lang/en/messages.php
@@ -1,19 +1,5 @@
 <?php
-
+// resources/lang/en/messages.php
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-    'name' => 'Name',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'copyright' => '© 2024 Portfolio Developed by Diego Saavedra León. All rights reserved.',
 ];

a8f9f2a5761e2bfee2ddd0e26921e927f7c6a5b2
Diego Saavedra León <dleon.desarrollo@gmail.com>
2024-01-21 23:44:23 -0300

    Arreglando llamado de controladores Login y Register para traer vistas desde route/auth

--- my-project/routes/auth.php
+++ my-project/routes/auth.php
@@ -5,13 +5,13 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 
 // Ruta para mostrar el formulario de registro
-Route::get('register', [RegisterController::class, 'create']);
+Route::get('register', [RegisterController::class, 'showRegister']);
 
 // Ruta para procesar petición de formulario de registro
 Route::post('register', [RegisterController::class, 'store']);
 
 // Ruta para mostrar el formulario de inicio de sesión
-Route::get('login', [LoginController::class, 'create']);
+Route::get('login', [LoginController::class, 'showLogin']);
 
 // Ruta para procesar petición de formulario de inicio de sesión
 Route::post('login', [LoginController::class, 'store']);

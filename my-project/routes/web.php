68971fb3a77817efc669d16257226357859a5c7d
Diego Saavedra León <dleon.desarrollo@gmail.com>
2024-01-21 21:38:07 -0300

    Agrupar rutas de autenticación en un grupo

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -1,31 +1,15 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisterController;
+// routes/web.php
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Ruta de la página de inicio
 Route::get('/', function () {
     return view('welcome');
 });
 
-/*
-Register web routes for your app's RouteServiceProvider 
-in a group containing the "web" middleware
-*/
+// Agrupar rutas de autenticación
+Route::prefix('auth')->group(function () {
+    require __DIR__.'/auth.php';
+});
 
-// Ruta para mostrar el formulario de registro
-Route::get('register', [RegisterController::class, 'create']);
-
-// Ruta para procesar el formulario de registro
-Route::post('register', [RegisterController::class, 'store']);
